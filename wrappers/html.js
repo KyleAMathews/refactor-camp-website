8c6f076d9f4f654b58ecc3627dceaa5d282e9ca2
Kyle Mathews <mathews.kyle@gmail.com>
2016-07-30 13:20:13 -0700

    HTML cleanups

--- wrappers/html.js
+++ wrappers/html.js
@@ -11,12 +11,7 @@ module.exports = React.createClass({
   render () {
     const post = this.props.route.page.data
     return (
-      <DocumentTitle title={`${config.siteTitle} | ${post.title}`}>
-        <div className="markdown">
-          <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-          <div dangerouslySetInnerHTML={{ __html: post.body }} />
-        </div>
-      </DocumentTitle>
+      <div dangerouslySetInnerHTML={{ __html: post.body }} />
     )
   },
 })
